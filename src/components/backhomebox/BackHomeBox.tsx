ef419ddcc0c913da8554fda9f81382fbd6aa050a
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-05-12 14:17:54 +0200

    bruk constant for tilbakeknapp

--- src/components/backhomebox/BackHomeBox.tsx
+++ src/components/backhomebox/BackHomeBox.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { Link as RouterLink } from "react-router";
 import { ArrowLeftIcon } from "@navikt/aksel-icons";
+import { ROOT } from "../../util/constant";
 import styles from "./BackHomeBox.module.css";
 
 type Props = {
@@ -8,14 +8,11 @@ type Props = {
   label?: string;
 };
 
-const BackHomeBox = ({
-  to = "/fastedata",
-  label = "Tilbake til oversikt",
-}: Props) => {
+const BackHomeBox = ({ to = ROOT, label = "Tilbake til oversikt" }: Props) => {
   return (
     <div className={styles.container}>
       <RouterLink to={to} className={styles.link}>
-        <ArrowLeftIcon aria-hidden style={{ marginRight: "0.5rem" }} />
+        <ArrowLeftIcon aria-hidden className={styles.ikon} />
         {label}
       </RouterLink>
     </div>
