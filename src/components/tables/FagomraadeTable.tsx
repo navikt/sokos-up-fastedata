17802d150e57828cf487a57fd20095494825dff4
Sebabebab <smedsrudsebastian@gmail.com>
2025-06-18 14:17:06 +0200

    mini fiks for filtrering

--- src/components/tables/FagomraadeTable.tsx
+++ src/components/tables/FagomraadeTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Pagination, Table } from "@navikt/ds-react";
 import { Fagomraader } from "../../types/Fagomraader";
 import { SortState, sortData } from "../../util/sortUtil";
@@ -22,6 +22,10 @@ export const FagomraadeTable = ({ data = [] }: Props) => {
     (currentPage - 1) * pageSize,
     currentPage * pageSize,
   );
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [data]);
 
   return (
     <>
