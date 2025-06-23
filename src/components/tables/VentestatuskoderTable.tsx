c02dfe37bb2205075440e46d7fedf459abb65018
Sebabebab <smedsrudsebastian@gmail.com>
2025-06-23 13:14:45 +0200

    lagt til paginering for ventekriterier og ventestatuskoder

--- src/components/tables/VentestatuskoderTable.tsx
+++ src/components/tables/VentestatuskoderTable.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Table } from "@navikt/ds-react";
+import { Pagination, Table } from "@navikt/ds-react";
+import commonstyles from "../../styles/Commonstyles.module.css";
 import { Ventestatuskoder } from "../../types/Ventestatuskoder";
 import { SortState, sortData } from "../../util/sortUtil";
 
@@ -11,6 +12,8 @@ const VentestatuskoderTable = ({ data = [] }: Props) => {
   const [sortState, setSortState] = useState<
     SortState<Ventestatuskoder> | undefined
   >();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize] = useState(10);
 
   const handleSortChange = (sortKey: string) => {
     const key = sortKey as keyof Ventestatuskoder;
@@ -24,42 +27,57 @@ const VentestatuskoderTable = ({ data = [] }: Props) => {
   };
 
   const sortedData = sortData(data, sortState);
+  const totalPages = Math.ceil(sortedData.length / pageSize);
+  const paginatedData = sortedData.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize,
+  );
 
   return (
-    <Table
-      zebraStripes
-      size="small"
-      sort={sortState}
-      onSortChange={handleSortChange}
-      className="ventestatuskoder-table"
-    >
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeader sortable sortKey="kodeVentestatus">
-            Ventestatuskode
-          </Table.ColumnHeader>
-          <Table.ColumnHeader sortable sortKey="beskrivelse">
-            Beskrivelse
-          </Table.ColumnHeader>
-          <Table.ColumnHeader>Prioritet</Table.ColumnHeader>
-          <Table.ColumnHeader>Settes manuelt</Table.ColumnHeader>
-          <Table.ColumnHeader>Kode arves til</Table.ColumnHeader>
-          <Table.ColumnHeader>Kan manuelt endres til</Table.ColumnHeader>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {sortedData.map((row, idx) => (
-          <Table.Row key={idx}>
-            <Table.DataCell>{row.kodeVentestatus}</Table.DataCell>
-            <Table.DataCell>{row.beskrivelse}</Table.DataCell>
-            <Table.DataCell>{row.prioritet}</Table.DataCell>
-            <Table.DataCell>{row.settesManuelt}</Table.DataCell>
-            <Table.DataCell>{row.kodeArvesTil}</Table.DataCell>
-            <Table.DataCell>{row.kanManueltEndresTil}</Table.DataCell>
+    <>
+      <Table
+        zebraStripes
+        size="small"
+        sort={sortState}
+        onSortChange={handleSortChange}
+        className="ventestatuskoder-table"
+      >
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader sortable sortKey="kodeVentestatus">
+              Ventestatuskode
+            </Table.ColumnHeader>
+            <Table.ColumnHeader sortable sortKey="beskrivelse">
+              Beskrivelse
+            </Table.ColumnHeader>
+            <Table.ColumnHeader>Prioritet</Table.ColumnHeader>
+            <Table.ColumnHeader>Settes manuelt</Table.ColumnHeader>
+            <Table.ColumnHeader>Kode arves til</Table.ColumnHeader>
+            <Table.ColumnHeader>Kan manuelt endres til</Table.ColumnHeader>
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+        </Table.Header>
+        <Table.Body>
+          {paginatedData.map((row, idx) => (
+            <Table.Row key={idx}>
+              <Table.DataCell>{row.kodeVentestatus}</Table.DataCell>
+              <Table.DataCell>{row.beskrivelse}</Table.DataCell>
+              <Table.DataCell>{row.prioritet}</Table.DataCell>
+              <Table.DataCell>{row.settesManuelt}</Table.DataCell>
+              <Table.DataCell>{row.kodeArvesTil}</Table.DataCell>
+              <Table.DataCell>{row.kanManueltEndresTil}</Table.DataCell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+      <div className={commonstyles["table-pagination-container"]}>
+        <Pagination
+          page={currentPage}
+          onPageChange={setCurrentPage}
+          count={totalPages}
+          size="small"
+        />
+      </div>
+    </>
   );
 };
 
