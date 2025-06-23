2a9d3cd554c4ccbf2e83ec69f94fb401db8fd10d
Sebabebab <smedsrudsebastian@gmail.com>
2025-06-23 10:13:20 +0200

    Fikset på måten fagomraader table bruker pagination

--- src/components/tables/FagomraadeTable.tsx
+++ src/components/tables/FagomraadeTable.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, Pagination, Table } from "@navikt/ds-react";
+import commonstyles from "../../styles/Commonstyles.module.css";
 import { Fagomraader } from "../../types/Fagomraader";
 import { SortState, sortData } from "../../util/sortUtil";
 import FagomraaderExpandableSection from "../expandablesections/FagomraaderExpandableSection";
@@ -8,16 +9,24 @@ import KorrigeringsarsakModal from "../modals/KorrigeringsarsakModal";
 
 interface Props {
   data?: Fagomraader[];
+  currentPage: number;
+  onPageChange: (page: number) => void;
 }
 
-export const FagomraadeTable = ({ data = [] }: Props) => {
+export const FagomraadeTable = ({
+  data = [],
+  currentPage,
+  onPageChange,
+}: Props) => {
   const [sort, setSort] = useState<SortState<Fagomraader> | undefined>();
   const [pageSize] = useState(10);
 
   const sortedData = sortData(data, sort);
-
   const totalPages = Math.ceil(sortedData.length / pageSize);
-  const paginatedData = sortedData.slice(0, pageSize);
+  const paginatedData = sortedData.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize,
+  );
 
   return (
     <>
@@ -84,14 +93,13 @@ export const FagomraadeTable = ({ data = [] }: Props) => {
           ))}
         </Table.Body>
       </Table>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "1rem",
-        }}
-      >
-        <Pagination page={1} count={totalPages} size="small" />
+      <div className={commonstyles["pagination-container"]}>
+        <Pagination
+          page={currentPage}
+          onPageChange={onPageChange}
+          count={totalPages}
+          size="small"
+        />
       </div>
     </>
   );
