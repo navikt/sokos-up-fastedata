41ef7fc4f3e1f2a3b8863b648b593124a8f13bd2
Sebabebab <smedsrudsebastian@gmail.com>
2025-08-04 11:03:50 +0200

    endret på filter og oppdatert til riktige versjoner

--- src/components/filters/klassekoderfilter/KlassekoderFilter.tsx
+++ src/components/filters/klassekoderfilter/KlassekoderFilter.tsx
@@ -1,21 +1,62 @@
-import { useState } from "react";
-import { BodyShort, Search } from "@navikt/ds-react";
+import { MagnifyingGlassIcon, XMarkIcon } from "@navikt/aksel-icons";
+import { BodyShort } from "@navikt/ds-react";
 import styles from "./KlassekoderFilter.module.css";
 
 const KlassekoderFilter = () => {
-  const [inputValue, setInputValue] = useState("");
-
   return (
     <div className={styles["filter-container"]}>
-      <BodyShort weight="semibold">Filtrer på klassekode</BodyShort>
+      <div className={styles["filter-group"]}>
+        <div className={styles["filter-field"]}>
+          <BodyShort size="small">Klassekode</BodyShort>
+          <div className={styles["input-wrapper"]}>
+            <input className={styles["input"]} type="text" />
+            <button className={styles["icon-button"]}>
+              <XMarkIcon />
+            </button>
+            <button className={styles["icon-button"]}>
+              <MagnifyingGlassIcon />
+            </button>
+          </div>
+        </div>
 
-      <div className={styles["search-container"]}>
-        <Search
-          label="Søk"
-          size="small"
-          value={inputValue}
-          onChange={(val) => setInputValue(val)}
-        />
+        <div className={styles["filter-field"]}>
+          <BodyShort size="small">Hovedkontonr</BodyShort>
+          <div className={styles["input-wrapper"]}>
+            <input className={styles["input"]} type="text" />
+            <button className={styles["icon-button"]}>
+              <XMarkIcon />
+            </button>
+            <button className={styles["icon-button"]}>
+              <MagnifyingGlassIcon />
+            </button>
+          </div>
+        </div>
+
+        <div className={styles["filter-field"]}>
+          <BodyShort size="small">Underkontonr</BodyShort>
+          <div className={styles["input-wrapper"]}>
+            <input className={styles["input"]} type="text" />
+            <button className={styles["icon-button"]}>
+              <XMarkIcon />
+            </button>
+            <button className={styles["icon-button"]}>
+              <MagnifyingGlassIcon />
+            </button>
+          </div>
+        </div>
+
+        <div className={styles["filter-field"]}>
+          <BodyShort size="small">Art-ID</BodyShort>
+          <div className={styles["input-wrapper"]}>
+            <input className={styles["input"]} type="text" />
+            <button className={styles["icon-button"]}>
+              <XMarkIcon />
+            </button>
+            <button className={styles["icon-button"]}>
+              <MagnifyingGlassIcon />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
