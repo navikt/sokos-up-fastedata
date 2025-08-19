422bf83d7efa71572347dff36235919e759685de
Lars Hartvigsen <lars.hartvigsen@nav.no>
2025-08-19 13:50:43 +0200

    Copy paste fra fagområder

---
+++ src/types/schema/FaggruppeSchema.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const FaggruppeSchema = z.object({
+  destinasjon: z.string(),
+  klassekodeFeil: z.string(),
+  klassekodeJustering: z.string(),
+  klassekodeMotpFeil: z.string(),
+  klassekodeMotpInnkr: z.string(),
+  klassekodeMotpTrekk: z.string(),
+  kodeFaggruppe: z.string(),
+  navnFaggruppe: z.string(),
+  oereavrunding: z.boolean(),
+  onlineBeregning: z.boolean(),
+  pensjon: z.boolean(),
+  prioritet: z.number(),
+  reskontroOppdrag: z.string(),
+  samordnetBeregning: z.string(),
+  skatteprosent: z.number(),
+  ventedager: z.number(),
+});
