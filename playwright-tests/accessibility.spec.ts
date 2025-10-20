803cfca342e731975f10c6ad2cda19fb3a6883c9
Sebabebab <smedsrudsebastian@gmail.com>
2025-10-20 11:07:57 +0200

    Lagt til at accesibility testen sjekker alle sidene til fastedata

--- playwright-tests/accessibility.spec.ts
+++ playwright-tests/accessibility.spec.ts
@@ -1,16 +1,26 @@
 import AxeBuilder from "@axe-core/playwright";
 import { expect, test } from "@playwright/test";
 
+const routes = [
+  "./fagrupper",
+  "./fagomraader",
+  "./ventestatuskoder",
+  "./klassekoder",
+  "./ventekriterier",
+];
+
 test.describe("Axe a11y", () => {
-  test("should not have any automatically detectable accessibility issues", async ({
-    page,
-  }) => {
-    await page.goto("/fastedata");
+  for (const path of routes) {
+    test(`should not have any automatically detectable accessibility issues on ${path}`, async ({
+      page,
+    }) => {
+      await page.goto(`/fastedata${path}`);
 
-    await page.waitForLoadState("networkidle");
+      await page.waitForLoadState("networkidle");
 
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+  }
 });
