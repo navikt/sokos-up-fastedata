c68c710891cde74d0c976cacc973c37d1a71f898
Sebabebab <smedsrudsebastian@gmail.com>
2025-10-28 12:31:10 +0100

    lagt til mere avanserte tester for klassekoder filteret

--- playwright-tests/klassekoder-filter.spec.ts
+++ playwright-tests/klassekoder-filter.spec.ts
@@ -22,15 +22,21 @@ test.describe("Klassekode filter", () => {
   }) => {
     await page.goto("/fastedata/klassekoder");
     await page.waitForLoadState("networkidle");
-
-    const klassekodeInput = page.getByLabel("Klassekode");
-    await klassekodeInput.fill("BANKSROCR");
-    await klassekodeInput.press("Enter");
+    const table = page.getByRole("table");
 
     const artIdInput = page.getByLabel("Art-ID");
     await artIdInput.fill("80");
-    await artIdInput.press("Enter");
+    await page.getByRole("option", { name: "80" }).click();
+
+    const klassekodeInput = page.getByLabel("Klassekode");
+    await klassekodeInput.fill("BANKSROCR");
+    await page.getByRole("option", { name: /Legg til.*BANKSROCR/i }).click();
+    await page.getByText("Faste data - Klassekoder").click();
 
     await expect(page.getByText("Ingen data tilgjengelig")).toBeVisible();
+
+    await page.getByRole("button", { name: "Art-ID: 80" }).click();
+
+    await expect(table.getByRole("cell", { name: "BANKSROCR" })).toBeVisible();
   });
 });
