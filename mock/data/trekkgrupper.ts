d856eef29020763ad5dabc5230b745f8106bc7a3
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2026-05-18 12:19:51 +0200

    legg til tester for trekkregler

--- mock/data/trekkgrupper.ts
+++ mock/data/trekkgrupper.ts
@@ -1,4 +1,6 @@
-export const trekkgrupper = [
+import type { Trekkgruppe } from "../../src/types/Trekkgruppe";
+
+export const trekkgrupper: Trekkgruppe[] = [
 	{
 		kodeTrekkgruppe: "AVRG",
 		kodeFagomraade: "MEFOGNY",
